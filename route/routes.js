d7e3e577fb727b314c7f2b3869280ce32f6aee76
Syaipuddin <muda3332@gmail.com>
2023-08-15 05:27:57 +0700

    roll-back to non-api

--- route/routes.js
+++ route/routes.js
@@ -18,35 +18,35 @@ import {
 } from '../controller/comment.js';
 
 // VIDEO
-router.get('/api/videos', getVideos);
-router.get('/api/video/:id', getVideoByID);
+router.get('/videos', getVideos);
+router.get('/video/:id', getVideoByID);
 
-router.post('/api/video/add', addVideo);
+router.post('/video/add', addVideo);
 
-router.patch('/api/video/add-product/:videoId/:productId', addProductToVideo);
-router.patch('/api/video/update/:id', updateVideo);
+router.patch('/video/add-product/:videoId/:productId', addProductToVideo);
+router.patch('/video/update/:id', updateVideo);
 
-router.delete('/api/video/delete/:id', deleteVideo);
-router.delete('/api/video/delete-product/:videoId/:productId', deleteProductFromVideo);
+router.delete('/video/delete/:id', deleteVideo);
+router.delete('/video/delete-product/:videoId/:productId', deleteProductFromVideo);
 
 // PRODUCT
-router.get('/api/products', getProducts);
-router.get('/api/product/:id', getProductByID);
+router.get('/products', getProducts);
+router.get('/product/:id', getProductByID);
 
-router.post('/api/product/add', upload.single('image'), addProduct);
+router.post('/product/add', upload.single('image'), addProduct);
 
-router.patch('/api/product/update/:id', upload.single('image'), updateProduct);
+router.patch('/product/update/:id', upload.single('image'), updateProduct);
 
-router.delete('/api/product/delete/:id', deleteProduct);
+router.delete('/product/delete/:id', deleteProduct);
 
 // COMMENT
-router.get('/api/comments', getComments);
-router.get('/api/comment/:id', getCommentByID);
+router.get('/comments', getComments);
+router.get('/comment/:id', getCommentByID);
 
-router.post('/api/comment/add', addComment);
+router.post('/comment/add', addComment);
 
-router.patch('/api/comment/update/:id', updateComment);
+router.patch('/comment/update/:id', updateComment);
 
-router.delete('/api/comment/delete/:id', deleteComment);
+router.delete('/comment/delete/:id', deleteComment);
 
 export default router;
